19185f619a0d90cfcbc29612af20e34ab75196ad
chengsurf <45204074+chengsurf@users.noreply.github.com>
2021-07-15 16:43:56 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,155 +1,149 @@
-package html
+package main
 
 import (
-	"strings"
-	"testing"
+    "database/sql"
+    "fmt"
+    gormSql "gorm.io/driver/mysql"
+    "gorm.io/gorm"
+    "log"
+    "strings"
 )
-
-type unescapeTest struct {
-	// A short description of the test case.
-	desc string
-	// The HTML text.
-	html string
-	// The unescaped text.
-	unescaped string
-}
-
-var unescapeTests = []unescapeTest{
-	// Handle no entities.
-	{
-		"copy",
-		"A\ttext\nstring",
-		"A\ttext\nstring",
-	},
-	// Handle simple named entities.
-	{
-		"simple",
-		"&amp; &gt; &lt;",
-		"& > <",
-	},
-	// Handle hitting the end of the string.
-	{
-		"stringEnd",
-		"&amp &amp",
-		"& &",
-	},
-	// Handle entities with two codepoints.
-	{
-		"multiCodepoint",
-		"text &gesl; blah",
-		"text \u22db\ufe00 blah",
-	},
-	// Handle decimal numeric entities.
-	{
-		"decimalEntity",
-		"Delta = &#916; ",
-		"Delta = Δ ",
-	},
-	// Handle hexadecimal numeric entities.
-	{
-		"hexadecimalEntity",
-		"Lambda = &#x3bb; = &#X3Bb ",
-		"Lambda = λ = λ ",
-	},
-	// Handle numeric early termination.
-	{
-		"numericEnds",
-		"&# &#x &#128;43 &copy = &#169f = &#xa9",
-		"&# &#x €43 © = ©f = ©",
-	},
-	// Handle numeric ISO-8859-1 entity replacements.
-	{
-		"numericReplacements",
-		"Footnote&#x87;",
-		"Footnote‡",
-	},
-	// Handle single ampersand.
-	{
-		"copySingleAmpersand",
-		"&",
-		"&",
-	},
-	// Handle ampersand followed by non-entity.
-	{
-		"copyAmpersandNonEntity",
-		"text &test",
-		"text &test",
-	},
-	// Handle "&#".
-	{
-		"copyAmpersandHash",
-		"text &#",
-		"text &#",
-	},
-}
-
-func TestUnescape(t *testing.T) {
-	for _, tt := range unescapeTests {
-		unescaped := UnescapeString(tt.html)
-		if unescaped != tt.unescaped {
-			t.Errorf("TestUnescape %s: want %q, got %q", tt.desc, tt.unescaped, unescaped)
-		}
-	}
-}
-
-func TestUnescapeEscape(t *testing.T) {
-	ss := []string{
-		``,
-		`abc def`,
-		`a & b`,
-		`a&amp;b`,
-		`a &amp b`,
-		`&quot;`,
-		`"`,
-		`"<&>"`,
-		`&quot;&lt;&amp;&gt;&quot;`,
-		`3&5==1 && 0<1, "0&lt;1", a+acute=&aacute;`,
-		`The special characters are: <, >, &, ' and "`,
-	}
-	for _, s := range ss {
-		if got := UnescapeString(EscapeString(s)); got != s {
-			t.Errorf("got %q want %q", got, s)
-		}
-	}
-}
 
 var (
-	benchEscapeData     = strings.Repeat("AAAAA < BBBBB > CCCCC & DDDDD ' EEEEE \" ", 100)
-	benchEscapeNone     = strings.Repeat("AAAAA x BBBBB x CCCCC x DDDDD x EEEEE x ", 100)
-	benchUnescapeSparse = strings.Repeat(strings.Repeat("AAAAA x BBBBB x CCCCC x DDDDD x EEEEE x ", 10)+"&amp;", 10)
-	benchUnescapeDense  = strings.Repeat("&amp;&lt; &amp; &lt;", 100)
+    gControllerDB *sql.DB /*控制台的db*/
+
+    gOrmDB *gorm.DB
 )
 
-func BenchmarkEscape(b *testing.B) {
-	n := 0
-	for i := 0; i < b.N; i++ {
-		n += len(EscapeString(benchEscapeData))
-	}
+type PolicyModel struct {
+    Id            int64 `gorm:"primaryKey"`
+    Name          string
+    Order         int
+
 }
 
-func BenchmarkEscapeNone(b *testing.B) {
-	n := 0
-	for i := 0; i < b.N; i++ 
+type teacher struct {
+    Id string `gorm:"primaryKey"`
+    Name string
+    Student []student
+
 }
 
-func BenchmarkUnescapeNone(b *testing.B) {
-	s := EscapeString(benchEscapeNone)
-	n := 0
-	for i := 0; i < b.N; i++ {
-		n += len(UnescapeString(s))
-	}
+type student struct {
+    Id uint `gorm:"primaryKey"`
+    Name string
+    TeacherId uint
 }
 
-func BenchmarkUnescapeSparse(b *testing.B) {
-	n := 0
-	for i := 0; i < b.N; i++ {
-		n += len(UnescapeString(benchUnescapeSparse))
-	}
+
+type RuleExprs struct {
+    PreOperator  string `json:"preOperator"`
+    DictId       string  `json:"dictId"`
+    DictName     string `json:"dictName"`
+    PostOperator string `json:"postOperator"`
+    Value        string `json:"value"`
 }
 
-func BenchmarkUnescapeDense(b *testing.B) {
-	n := 0
-	for i := 0; i < b.N; i++ {
-		n += len(UnescapeString(benchUnescapeDense))
-	}
+
+func InitDB() error {
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", "root", "xy_123456", "127.0.0.1", 3306, "test")
+    log.Print(dsn)
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        log.Print(err)
+        return err
+    }
+
+    err = db.Ping()
+    if err != nil {
+        log.Print(err)
+        return err
+    }
+
+    db.SetMaxOpenConns(128)
+    db.SetMaxIdleConns(32)
+
+    gControllerDB = db
+
+    gOrmDB, err = gorm.Open(gormSql.New(gormSql.Config{Conn: gControllerDB}), &gorm.Config{CreateBatchSize: 100,SkipDefaultTransaction: false,})
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
+
+type Conditions struct {
+    Operator string `json:"operator"'`
+    Value    string `json:"value"'`
+}
+func GetBetweenStr(str, start, end string) string {
+    n := strings.Index(str, start)
+    if n == -1 {
+       return ""
+    }
+
+
+    str = string([]byte(str)[n + len(start):])
+    m := strings.Index(str, end)
+    if m == -1 {
+        m = len(str)
+    }
+    str = string([]byte(str)[:m])
+    return str
+}
+
+
+func main() {
+    InitDB()
+    user := teacher{
+        Id:   "3",
+        Name: "jinzhu111",
+        Student: []student{
+            {Id: 4,
+                Name: "jinzhu@example.com"},
+        },
+    }
+    gOrmDB.Updates(&user)
+
+    var t teacher
+
+    var param map[string]interface{} = make(map[string]interface{})
+    param["name"] = gorm.Expr("unix_timestamp(now(6)) * ?", 100000)
+
+    db := gOrmDB.Table("teachers")
+    res := db.Where("id in (?)", []string{"2"}).Updates(param)
+    if res.Error != nil {
+        log.Printf("11112%v", res.Error)
+    }
+
+    gOrmDB.Table("teachers").Where("id in (?)", []string{"2"}).Find(&t)
+  ///  log.Printf("11112%v", t)
+
+    //sql := "update teachers set id = unix_timestamp(now(6)) * 100000 where id =2"
+    //gControllerDB.Exec(sql)
+    //gOrmDB.Table("teachers").Where("id in (?)", []string{"2"}).Find(&t)
+    //log.Printf("11133%v",t)
+
+    //policy := PolicyModel{2, "114", 2}
+    //var policyList []PolicyModel
+    //policyList = append(policyList, PolicyModel{1, "111", 1})
+    //policyList = append(policyList, PolicyModel{2, "112", 2})
+    //policyList = append(policyList, PolicyModel{3, "113", 3})
+    //
+    //p := updatePolicyFilter(&policy,policyList)
+    //log.Printf("11111%v",policy)
+    //log.Printf("11112%v",p)
+
+   // body := "[{\"id\":1,\"fileKey\":\"210712051905zecelv\",\"fileName\":\"main.go\",\"contentType\":\"application/octet-stream\",\"size\":3637,\"fileUrl\":\"https://webmail30.189.cn/w2/mail/previewImage.do?fileKey\\u003d210712051905zecelv%2Fmain.go\",\"code\":0}]"
+    privValue :=0x00000001|0x00000020
+    privValueInDB:=0x00000020
+
+    log.Printf("11112 %v",(privValue & privValueInDB))
+
+
+
+}
+
+
+
